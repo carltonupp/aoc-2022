93053cd5f02be80ea122acfa4819d509486010a0
Carlton Upperdine <carlton.upperdine@gmail.com>
2022-12-07 17:56:09 +0000

    eod

--- src/3/rucksackreorganisation/lib/rucksackreorganisation.ex
+++ src/3/rucksackreorganisation/lib/rucksackreorganisation.ex
@@ -2,17 +2,44 @@ defmodule RucksackReorganisation do
   @moduledoc """
   Documentation for `RucksackReorganisation`.
   """
+alias Supervisor.Default
 
   @doc """
   Hello world.
 
   ## Examples
 
-      iex> Rucksackreorganisation.hello()
-      :world
 
   """
-  def hello do
-    :world
+
+  def split_string_in_half(str) do
+    halfway = round(String.length(str) / 2)
+    first = String.slice(str, 0..halfway - 1)
+    second = String.slice(str, halfway, halfway)
+    { first, second }
   end
+
+  def find_overlaps(substrings) do
+    {str1, str2} = substrings
+    Enum.filter(String.graphemes(str1), fn c -> Enum.member?(String.graphemes(str2), c) end)
+    |> Enum.take(1)
+  end
+
+  def intersect(list1, list2) do
+    Enum.filter(list1, fn x -> Enum.member?(list2, x) end)
+  end
+
+  def get_char_value(char) do
+    charset = String.graphemes("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    (Enum.find_index(charset, fn c -> c == char end) || 0) + 1
+  end
+
+  def sum_of_priorites rucksacks do
+    rucksacks
+    |> Enum.map(fn str -> split_string_in_half(str) end)
+    |> Enum.map(fn strs -> find_overlaps(strs) end)
+    |> Enum.flat_map(fn x -> get_char_value(x) end)
+    |> Enum.sum()
+  end
+
 end
--- src/3/rucksackreorganisation/test/rucksackreorganisation_test.exs
+++ src/3/rucksackreorganisation/test/rucksackreorganisation_test.exs
@@ -1,8 +1,29 @@
 defmodule RucksackReorganisationTest do
   use ExUnit.Case
-  doctest Rucksackreorganisation
+  doctest RucksackReorganisation
 
-  test "greets the world" do
-    assert RucksackReorganisation.hello() == :world
+  test "part 1 sample inputs" do
+    samples = [
+      "vJrwpWtwJgWrhcsFMMfFFhFp",
+      "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+      "PmmdzqPrVvPwwTWBwg",
+      "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+      "ttgJtRGJQctTZtZT",
+      "CrZsJsPPZsGzwwsLwLmpwMDw"]
+
+    assert RucksackReorganisation.sum_of_priorites(samples) == 157
+  end
+
+  test "split string works" do
+    assert elem(RucksackReorganisation.split_string_in_half("vJrwpWtwJgWrhcsFMMfFFhFp"), 0) == "vJrwpWtwJgWr"
+    assert elem(RucksackReorganisation.split_string_in_half("vJrwpWtwJgWrhcsFMMfFFhFp"), 1) == "hcsFMMfFFhFp"
+  end
+
+  test "overlaps" do
+    values = RucksackReorganisation.split_string_in_half("vJrwpWtwJgWrhcsFMMfFFhFp")
+    |> RucksackReorganisation.find_overlaps()
+
+    # assert values == [1, 2, 3]
+    assert RucksackReorganisation.intersect([1, 2, 3], [2, 3, 4]) == [2, 3]
   end
 end
