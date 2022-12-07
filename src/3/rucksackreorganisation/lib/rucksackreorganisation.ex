5de6885684840c557559eb765cca5d27aa777d35
Carlton Upperdine <carlton.upperdine@gmail.com>
2022-12-07 16:28:56 +0000

    scaffold

---
+++ src/3/rucksackreorganisation/lib/rucksackreorganisation.ex
@@ -0,0 +1,18 @@
+defmodule RucksackReorganisation do
+  @moduledoc """
+  Documentation for `RucksackReorganisation`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rucksackreorganisation.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ src/3/rucksackreorganisation/test/rucksackreorganisation_test.exs
@@ -0,0 +1,8 @@
+defmodule RucksackReorganisationTest do
+  use ExUnit.Case
+  doctest Rucksackreorganisation
+
+  test "greets the world" do
+    assert RucksackReorganisation.hello() == :world
+  end
+end
