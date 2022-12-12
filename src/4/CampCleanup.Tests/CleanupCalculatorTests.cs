4583c6ef04d7e26a358a437a46d9730d6e389b6e
Carlton Upperdine <carlton.upperdine@gmail.com>
2022-12-12 17:10:01 +0000

    merge branch day-4 into main

---
+++ src/4/CampCleanup.Tests/CleanupCalculatorTests.cs
@@ -0,0 +1,72 @@
+namespace CampCleanup.Tests;
+
+public class CleanupCalculatorTests
+{
+    [Fact]
+    public void Part1_SampleInputs()
+    {
+        var inputs = new List<Tuple<SectionRange, SectionRange>>
+        {
+            new(new SectionRange(2, 4), new SectionRange(6, 8)),
+            new(new SectionRange(2, 3), new SectionRange(4, 5)),
+            new(new SectionRange(5, 7), new SectionRange(7, 9)),
+            new(new SectionRange(2, 8), new SectionRange(3, 7)),
+            new(new SectionRange(6, 6), new SectionRange(4, 6)),
+            new(new SectionRange(2, 6), new SectionRange(4, 8))
+        };
+        
+        Assert.Equal(2, CleanupCalculator.GetFullyOverlappingRanges(inputs));
+    }
+
+    [Fact]
+    public void Part1_RealInputs()
+    {
+        var inputs = File.ReadLines("./inputs.txt")
+            .Select(TurnLineIntoSectionRangeTuple);
+        
+        Assert.Equal(496, CleanupCalculator.GetFullyOverlappingRanges(inputs));
+    }
+
+    [Fact]
+    public void Part2_SampleInputs()
+    {
+        var inputs = new List<Tuple<SectionRange, SectionRange>>
+        {
+            new(new SectionRange(2, 4), new SectionRange(6, 8)),
+            new(new SectionRange(2, 3), new SectionRange(4, 5)),
+            new(new SectionRange(5, 7), new SectionRange(7, 9)),
+            new(new SectionRange(2, 8), new SectionRange(3, 7)),
+            new(new SectionRange(6, 6), new SectionRange(4, 6)),
+            new(new SectionRange(2, 6), new SectionRange(4, 8))
+        };
+        
+        Assert.Equal(4, CleanupCalculator.GetPartiallyOverlappingRanges(inputs));
+    }
+
+    [Fact]
+    public void Part2_RealInputs()
+    {
+        var inputs = File.ReadLines("./inputs.txt")
+            .Select(TurnLineIntoSectionRangeTuple);
+        
+        Assert.Equal(847, CleanupCalculator.GetPartiallyOverlappingRanges(inputs));
+    }
+
+    private static Tuple<SectionRange, SectionRange> TurnLineIntoSectionRangeTuple(string line)
+    {
+        return line.Split(',').Select(TurnSubStringIntoSectionRange).ToArray() switch
+        {
+            [var first, var last] => new Tuple<SectionRange, SectionRange>(first, last),
+            _ => throw new Exception()
+        };
+    }
+
+    private static SectionRange TurnSubStringIntoSectionRange(string str)
+    {
+        return str.Split('-').Select(int.Parse).ToArray() switch
+        {
+            [var start, var end] => new SectionRange(start, end),
+            _ => throw new Exception()
+        };
+    }
+}
---
+++ src/4/CampCleanup/CleanupCalculator.cs
@@ -0,0 +1,32 @@
+﻿namespace CampCleanup;
+
+public static class CleanupCalculator
+{
+    public static int GetFullyOverlappingRanges(IEnumerable<Tuple<SectionRange, SectionRange>> ranges) =>
+        ranges.Count(r =>
+        {
+            var (common, smallest) = (r.Item1.Length, r.Item2.Length) switch
+            {
+                var (r1, r2) when r1 > r2 => new Tuple<IEnumerable<int>, IEnumerable<int>>(
+                    IntersectSectionRanges(r.Item1, r.Item2), r.Item2.ToEnumerable()),
+                var (r1, r2) when r2 > r1 => new Tuple<IEnumerable<int>, IEnumerable<int>>(
+                    IntersectSectionRanges(r.Item2, r.Item1), r.Item1.ToEnumerable()),
+                _ => new Tuple<IEnumerable<int>, IEnumerable<int>>(IntersectSectionRanges(r.Item1, r.Item2),
+                    r.Item2.ToEnumerable())
+            };
+
+            return common.Count() == smallest.Count()
+                   && !common.Except(smallest).Any();
+        });
+
+    public static int GetPartiallyOverlappingRanges(IEnumerable<Tuple<SectionRange, SectionRange>> ranges) =>
+        ranges.Count(r => r.Item1.ToEnumerable()
+            .Intersect(r.Item2.ToEnumerable())
+            .Any());
+
+    private static IEnumerable<int> IntersectSectionRanges(SectionRange largest, SectionRange smallest)
+    {
+        return largest.ToEnumerable().Intersect(
+            smallest.ToEnumerable());
+    }
+}
